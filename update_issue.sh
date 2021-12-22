d101e09ee56264af7c634be61ecea7d13d79dc0d
valtteriviirret <viirretvaltteri@gmail.com>
2021-12-22 08:00:06 +0200

    update_issue.sh

--- update_issue.sh
+++ update_issue.sh
@@ -1,27 +1,43 @@
 #!/usr/bin/env bash
 
-name='Valtteri\n'
+name="Valtteri\n\n"
 time="10#$(date +'%H')"
+day="$(date +'%a')"
 
 # night
 if [[ ${time} -ge 0 && ${time} -lt 6 ]]; then
-	timeOfDay='Hyvää yötä, '
+	timeOfDay='Hyvää yötä '
 fi
 
 # morning
 if [[ ${time} -ge 6 && ${time} -lt 12 ]]; then
-	timeOfDay='Hyvää huomenta, '
+	timeOfDay='Hyvää huomenta '
 fi
 
 # day
 if [[ ${time} -ge 12 && ${time} -lt 18 ]]; then
-	timeOfDay='Hyvää päivää, '
+	timeOfDay='Hyvää päivää '
 fi
 
 # evening
 if [[ ${time} -ge 18 && ${time} -lt 24 ]]; then
-	timeOfDay='Hyvää iltaa, '
+	timeOfDay='Hyvää iltaa '
 fi
 
-echo -e ${timeOfDay}${name} > /etc/issue
+# weekday
+case ${day} in
+	Mon) myday="maanantai" ;;
+	Tue) myday="tiistai" ;;
+	Wed) myday="keskiviikko" ;;
+	Thu) myday="torstai" ;;
+	Fri) myday="perjantai" ;;
+	Sat) myday="lauantai" ;;
+	Sun) myday="sunnuntai" ;;
+	*)
+esac
+
+todayis="Tänään on "
+nums=" $(date +'%d').$(date +'%m')\n"
+
+echo -e ${timeOfDay}${name}${todayis}${myday}${nums} > /etc/issue
 
