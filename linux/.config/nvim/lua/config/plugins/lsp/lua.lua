a2836b2454d40c8a91b057c17d847aaf7a9c3eb0
viirret <viirretvaltteri@gmail.com>
2025-12-27 16:49:19 +0200

    Fix lua lsp before creating lspbuilder

--- linux/.config/nvim/lua/config/plugins/lsp/lua.lua
+++ linux/.config/nvim/lua/config/plugins/lsp/lua.lua
@@ -1,12 +1,18 @@
 -- lua/lsp/lua.lua
 local M = {}
 
+local function lua_root(fname)
+	local root_util = require("utils.root_dir")
+	return root_util.get_root_dir(fname, { "luarc.json", ".git", "init.lua" })
+end
+
 function M.setup(capabilities, on_attach)
 	vim.lsp.config["lua_ls"] = {
 		cmd = { "lua-language-server" },
 		capabilities = capabilities,
 		on_attach = on_attach,
 		filetypes = { "lua" },
+		root_dir = lua_root(),
 		settings = {
 			Lua = {
 				runtime = {
@@ -28,15 +34,15 @@ function M.setup(capabilities, on_attach)
 		pattern = { "lua" },
 		callback = function(args)
 			local active = vim.lsp.get_clients({ bufnr = args.buf, name = "lua_ls" })
+			local root = lua_root()
+
 			if #active == 0 then
 				vim.lsp.start({
 					name = "lua_ls",
 					cmd = { "lua-language-server" },
 					capabilities = capabilities,
 					on_attach = on_attach,
-					root_dir = vim.fs.dirname(
-						vim.fs.find({ ".luarc.json", ".git" }, { upward = true, path = args.file })[1]
-					),
+					root_dir = root,
 					settings = {
 						Lua = {
 							runtime = { version = "LuaJIT" },
